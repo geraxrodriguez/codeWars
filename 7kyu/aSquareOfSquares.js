644961908c70d856236133482be84972b51b8b15
Gerardo Rodriguez <gerarodriguez@proton.me>
2023-08-16 23:39:01 -0500

    solved aSquareOfSquares

--- 7kyu/aSquareOfSquares.js
+++ 7kyu/aSquareOfSquares.js
@@ -5,3 +5,10 @@
 //  3  =>  false
 // 25  =>  true
 // 26  =>  false
+
+// The Math.sqrt() static method returns the square root of a number.
+var isSquare = function(n){
+    // The Math.sqrt() static method returns the square root of a number.
+    // Number.isInteger returns true/false depending if number is, well, an integer
+    return Number.isInteger(Math.sqrt(n)) 
+}
