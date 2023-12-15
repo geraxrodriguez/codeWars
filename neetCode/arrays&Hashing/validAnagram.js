1ca0ea23be06b99a06da75681d1686c9c4922adc
Gerardo Rodriguez <gerarodriguez@proton.me>
2023-12-15 10:27:54 -0600

    commented out solutions for Valid Anagram

--- neetCode/arrays&Hashing/validAnagram.js
+++ neetCode/arrays&Hashing/validAnagram.js
@@ -20,6 +20,8 @@
  * @return {boolean}
  */
 //  first solution
+// if you compare two arrays with same elements using '==' or '===', it will return false because the two arrays reference different objects in memory.
+// by joining them into strings, you then compare the contents, resulting in true if they have same contents
 var isAnagram = function(s, t) {
     return sortString(s) == sortString(t)
 };
@@ -29,8 +31,14 @@ var sortString = function(str) {
 
 // second solution, runs bit faster and is better on memory. Requires hashing
 var isAnagram = function(s, t) {
+    //if both strs aren't same length, we know immediately they aren't anagrams
     if (s.length !== t.length) { return false }
 
+    //if we are concerned with occurrences of something, such as in this case
+    //then we NEED to create a map
+    //here we are creating a map and doing one of two things:
+    //1. creating an element in our map if it doesn't exist there yet
+    //2. if it does exist, we increment its occurence by +1
     let map = {}
     for (let i = 0; i < s.length; i++) {
         let letter = s[i]
@@ -41,6 +49,7 @@ var isAnagram = function(s, t) {
         }
     }
 
+    //looping through our second str
     for (let i = 0; i < t.length; i++) {
         let letter = t[i]
         // if letter don't exist in map, false
@@ -48,13 +57,12 @@ var isAnagram = function(s, t) {
             return false
         }
         // if letter already at 0, false 
-        else if (map[letter] == 0) {
+        if (map[letter] == 0) {
             return false
         }
         // else subtract 1 from letter value 
-        else { 
-            map[letter]--
-        }
+        map[letter]--
+
     } 
 
     return true
