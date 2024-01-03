d7e9673f812cf2ae7bd63a363abfd866d78a7e5a
Gerardo Rodriguez <gerarodriguez@proton.me>
2024-01-03 13:49:43 -0600

    commented out a better solution for reverseOrRotates.js

--- 6kyu/reverseOrRotates.js
+++ 6kyu/reverseOrRotates.js
@@ -51,25 +51,40 @@ function revrot(str, sz) {
   }
 // not mine but clean, key difference is they only looped through str once. that's amazing
 function revrot(str, sz) {
+    
+    // if either params aren't provided, or if sz > str then we can't make a chunk
     if (sz <= 0 || !str || sz > str.length) return '';    
-      
+    
+    // split each chunk up, cube each digit, and add it to our sum
     const sumCubes = chunk => chunk.split('').reduce((sum, digit) => sum += digit**3, 0);
+    // split each chunk, reverse its order and rejoin
     const reverse = chunk => chunk.split('').reverse().join('');
+    // remove first digit and add it to the end
     const rotate = chunk => chunk.slice(1) + chunk.slice(0, 1);
     
+    // empty placeholder array
     const chunks = [];
     
+    // loop through str by our sz parameter
     for (let i = 0; i < str.length; i += sz) {
+      
+      // setting chunk variable to substring starting at current i all way to next sz digits
       let chunk = str.slice(i, i + sz);
       
+      // if chunk length not equal to what our chuck sz should be then move on to next iteration
       if (chunk.length < sz) continue;
       
+      // calling our sumCubes func passing in our chunk and seeing if the result is divisble by 2
       chunk = sumCubes(chunk) % 2 
+        // if is, then we call our rotate func, passing in chunk, and that will be our new chunk value
         ? rotate(chunk)   
+        // if not, our new chunk val will be whatever our reverse func returns
         : reverse(chunk);
       
+      // push it to our placeholder array
       chunks.push(chunk)
     }
     
+    // join our placeholder array
     return chunks.join('')
 }
