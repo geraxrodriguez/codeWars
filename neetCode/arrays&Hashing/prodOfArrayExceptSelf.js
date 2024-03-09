92598df116d6e943c01b99603f8c556a37410a1d
Gerardo Rodriguez <gerarodriguez@proton.me>
2024-03-09 11:47:17 -0600

    re-solved Product of Array Except Self

--- neetCode/arrays&Hashing/prodOfArrayExceptSelf.js
+++ neetCode/arrays&Hashing/prodOfArrayExceptSelf.js
@@ -20,3 +20,19 @@ var productExceptSelf = function(nums) {
     }
     return answers
 };
+
+var productExceptSelf = function(nums) {
+    let forwardArr = [];
+    let start = 1;
+    for (let i = 0; i < nums.length; i++) {
+        forwardArr.push(start);
+        start = start * nums[i]
+    }
+    let res = [];
+    let start2 = 1;
+    for (let i = nums.length - 1; i >= 0; i--) {
+        res.unshift(start2 * forwardArr[i]);
+        start2 = start2 * nums[i];
+    }
+    return res;
+}
