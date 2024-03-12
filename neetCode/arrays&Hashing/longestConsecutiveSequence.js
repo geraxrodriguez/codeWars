df834d4630ac06a3b4e41ded53c194e91cebefd0
Gerardo Rodriguez <gerarodriguez@proton.me>
2024-03-12 09:45:08 -0500

    solved Longest Consecutive Streak Again

--- neetCode/arrays&Hashing/longestConsecutiveSequence.js
+++ neetCode/arrays&Hashing/longestConsecutiveSequence.js
@@ -49,3 +49,25 @@ var longestConsecutive = function(nums) {
     return Math.max(max, count)
 };
 // 3/10
+
+var longestConsecutive = function(nums) {
+    if (nums == null || nums.length === 0) { return 0 };
+    const set = new Set(nums);
+    let max = 0;
+    
+    for (let num of set) {
+        if (set.has(num - 1)) { continue; } // if current num not start of a sequence
+        
+        let currNum = num;
+        let currMax = 1;
+        while (set.has(currNum + 1)) {
+            currNum++;
+            currMax++;
+        }
+
+        max = Math.max(max, currMax);
+    }
+
+    return max
+}
+// 3/12
