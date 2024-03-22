8d48af6b2a3cda4354d4a4b741d4492b36339f77
Gerardo Rodriguez <gerarodriguez@proton.me>
2024-03-22 10:04:39 -0500

    created Python folder for NeetCode python solutions, moved JS solutions into JS folder

---
+++ neetCode/Python/containsDuplicates.py
@@ -0,0 +1,25 @@
+class Solution(object):
+    def containsDuplicate(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: bool
+        """
+        # create map
+        seen = {}
+
+        # iterate through each num in nums
+        for num in nums:
+            if num in seen and seen[num] >= 1:
+                return True
+            seen[num] = seen.get(num, 0) + 1
+        return False
+
+        # above can be refracoted to this
+        for num in nums:
+            if num in seen:
+                return True
+            seen[num] = 1
+        return False
+
+        
+    
