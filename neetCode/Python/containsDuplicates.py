93abbca0a793dac9cc50b953df298d2c45e9ad55
Gerardo Rodriguez <gerarodriguez@proton.me>
2024-03-23 10:23:03 -0500

    new solution for containsDuplicates.py

--- neetCode/Python/containsDuplicates.py
+++ neetCode/Python/containsDuplicates.py
@@ -22,4 +22,17 @@ class Solution(object):
         return False
 
         
-    
+class Solution(object):
+    def containsDuplicate(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: bool
+        """
+        # create map
+        seen = set()
+
+        for num in nums:
+            if num in seen:
+                return True
+            seen.add(num)
+        return False
