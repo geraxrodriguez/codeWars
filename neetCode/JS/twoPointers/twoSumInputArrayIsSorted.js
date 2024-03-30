44df80853043ce7cfc6c9704bdaaeb02fd33fc6f
Gerardo Rodriguez <gerarodriguez@proton.me>
2024-03-30 12:52:06 -0500

    solution provided for Two Sum || Input Array is Sorted

---
+++ neetCode/JS/twoPointers/twoSumInputArrayIsSorted.js
@@ -0,0 +1,16 @@
+var twoSum = function(numbers, target) {
+    let p1 = 0;
+    let p2 = numbers.length - 1
+    while (p1 < p2) {
+        const sum = numbers[p1] + numbers[p2]
+        if (sum == target){
+            return [p1 + 1, p2 + 1]
+        }
+        else if (sum < target) {
+            p1 += 1
+        }
+        else {
+            p2 -= 1
+        }
+    }
+};
