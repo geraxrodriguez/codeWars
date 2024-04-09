ee9692d90fbf64f47f386120fccf1fbf192da744
Gerardo Rodriguez <gerarodriguez@proton.me>
2024-04-08 20:22:24 -0500

    solved Container With Most Water

---
+++ neetCode/JS/twoPointers/containerWithMost.js
@@ -0,0 +1,16 @@
+var maxArea = function(height) {
+    let max = 0;
+    for (let i = 0; i < height.length; i++){
+        let bar1 = i;
+        let bar2 = i + 1
+        while (bar2 < height.length) {
+            const height1 = height[bar1]
+            const height2 = height[bar2]
+            const water = Math.min(height1, height2) * Math.abs(bar1 - bar2)
+            max = Math.max(max, water)
+            bar2 += 1;
+        }
+    }
+
+    return max
+}
